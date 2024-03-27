b6dcf016964b1a50165bada1376a0528ffbffb67
henry <henry.aspeti@gmail.com>
2024-03-27 11:47:40 -0400

    projectManager

---
+++ src/main/java/com/dharbor/talent/managervacations/ManagervacationsApplication.java
@@ -0,0 +1,13 @@
+package com.dharbor.talent.managervacations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagervacationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagervacationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dharbor/talent/managervacations/ManagervacationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharbor.talent.managervacations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagervacationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
